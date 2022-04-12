2d9ee147ecf91512ea2c9d8ace9e21fee9cc4cb1
Berat Yavuz <beratttyavuzz@gmail.com>
2022-04-12 11:52:05 +0300

    Initial Commit

---
+++ SayiTahminApp/SayiTahminAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayiTahminAppApp.swift
+//  SayiTahminApp
+//
+//  Created by Berat Yavuz on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SayiTahminAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
